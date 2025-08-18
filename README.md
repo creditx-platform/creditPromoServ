73c651869908b6398f6dbf9a3b80faa5d2748c8b
AkshayKappala <akshaykappala98@gmail.com>
2025-08-18 17:51:28 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Promotion Service for CreditX Platform
+Evaluates eligible cashback, discounts, and promotions
---
+++ src/main/java/com/creditx/promo/CreditPromoServApplication.java
@@ -0,0 +1,13 @@
+package com.creditx.promo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditPromoServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditPromoServApplication.class, args);
+	}
+
+}
